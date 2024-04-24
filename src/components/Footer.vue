28bc279d80d51a230ac35675777441e3ba7f44f1
ecpieritz <ecpieritz@gmail.com>
2024-04-24 12:03:07 -0300

    feat: add footer - part 2

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,32 +3,38 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-md-5 col-12 vt-footer__logo">
-          <img class="mb-4" src="@/assets/imgs/vt-logo.png" alt="VueTech">
-          <p>VueTech is a leading digital marketing agency dedicated to driving success for businesses online. With expertise in SEO, PPC, social media, content creation, and more, we offer tailored solutions to maximize ROI.</p>
+          <img class="mb-4" src="@/assets/imgs/vt-logo.png" alt="VueTech" />
+          <p>
+            VueTech is a leading digital marketing agency dedicated to driving success for
+            businesses online. With expertise in SEO, PPC, social media, content creation,
+            and more, we offer tailored solutions to maximize ROI.
+          </p>
         </div>
 
-        <div class="col-md-2 col-6">
+        <div class="col-md-2 col-12">
           <h4 class="mb-5">Our Services</h4>
           <ul>
             <li v-for="(service, index) in services" :key="index">{{ service }}</li>
           </ul>
         </div>
 
-        <div class="col-md-2 col-6">
+        <div class="col-md-2 col-12">
           <h4 class="mb-5">Explore More</h4>
           <ul>
-            <li v-for="(link, index) in links" :key="index"><a :href="link.href">{{ link.title }}</a></li>
+            <li v-for="(link, index) in links" :key="index">
+              <a :href="link.href">{{ link.title }}</a>
+            </li>
           </ul>
         </div>
-        
-        <div class="col-md-3 col-6">
+
+        <div class="col-md-3 col-12">
           <h4 class="mb-5">Contact Us</h4>
 
           <div class="vt-footer__infos row">
             <div v-for="(info, index) in infos" :key="index" class="col-12">
               <div class="d-flex align-items-center">
                 <div class="col-3">
-                  <img :src="info.src" :alt="info.alt">
+                  <img :src="info.src" :alt="info.alt" />
                 </div>
                 <div class="col-9">
                   <p>{{ info.text }}</p>
@@ -37,6 +43,19 @@
             </div>
           </div>
         </div>
+      </div>
+      <div class="vt-footer__made-by">
+        <p>VueTech 2024 © All right reserved</p>
+
+        <p>
+          Made with 💙 by
+          <a
+            href="https://www.linkedin.com/in/ecpieritz/"
+            target="_blank"
+            rel="noopener noreferrer"
+            >Emilyn Pieritz</a
+          >
+        </p>
       </div>
     </div>
   </section>
@@ -54,20 +73,32 @@ export default {
         "Google PPC",
       ],
       links: [
-        { href: '#vt-services', title: 'Services' },
-        { href: '#vt-choose', title: 'Choose Us' },
-        { href: '#vt-prices', title: 'Prices' },
-        { href: '#vt-contact', title: 'Contact' },
-        { href: '/', title: 'Privacy Policy' },
-        { href: '/', title: 'Terms & Condition' },
+        { href: "#vt-services", title: "Services" },
+        { href: "#vt-choose", title: "Choose Us" },
+        { href: "#vt-prices", title: "Prices" },
+        { href: "#vt-contact", title: "Contact" },
+        { href: "/", title: "Privacy Policy" },
+        { href: "/", title: "Terms & Condition" },
       ],
       infos: [
-        { src: require('@/assets/imgs/icons/vt-icon__local.png'), alt: 'local', text: '5th Street, Ananindeua - Brazil' },
-        { src: require('@/assets/imgs/icons/vt-icon__phone.png'), alt: 'phone', text: '+55 91 3456-7891' },
-        { src: require('@/assets/imgs/icons/vt-icon__cellphone.png'), alt: 'cellphone', text: '+55 91 98765-4321' },
-      ]
+        {
+          src: require("@/assets/imgs/icons/vt-icon__local.png"),
+          alt: "local",
+          text: "5th Street, Ananindeua - Brazil",
+        },
+        {
+          src: require("@/assets/imgs/icons/vt-icon__phone.png"),
+          alt: "phone",
+          text: "+55 91 3456-7891",
+        },
+        {
+          src: require("@/assets/imgs/icons/vt-icon__cellphone.png"),
+          alt: "cellphone",
+          text: "+55 91 98765-4321",
+        },
+      ],
     };
-  }
+  },
 };
 </script>
 
@@ -86,22 +117,22 @@ $vt-white: #ffffff;
   color: $vt-white;
   padding: 80px 0 0;
 
-  &__logo{
-    img{
+  &__logo {
+    img {
       width: 37%;
     }
   }
 
-  h4{
+  h4 {
     color: $vt-yellow;
   }
 
-  ul{
-    list-style-type: none; 
-    li{
+  ul {
+    list-style-type: none;
+    li {
       font-size: 20px;
       margin-bottom: 5px;
-      a{
+      a {
         font-size: 20px;
         font-weight: 400;
         color: $vt-white;
@@ -113,18 +144,33 @@ $vt-white: #ffffff;
     }
   }
 
-  &__infos{
-
-    .col-12{
+  &__infos {
+    .col-12 {
       margin-bottom: 20px;
 
-      p{
+      p {
         margin: 0;
       }
     }
-    img{
+    img {
       width: 100%;
     }
   }
+
+  &__made-by {
+    border-top: 2px solid $vt-white;
+    margin-top: 40px;
+    padding: 40px 40px 20px;
+    display: flex;
+    justify-content: space-between;
+
+    a{
+      font-size: 20px;
+      color: $vt-yellow;
+    }
+
+    
+  }
+  
 }
 </style>
